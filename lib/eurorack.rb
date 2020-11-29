12b9c3394c0da607a9a1841dd4cbffbe8c6c0c74
Joe Miragliuolo <sod@llawn.com>
2020-11-28 22:19:05 -0500

    Add method documentation and minor tweaks

--- lib/eurorack.rb
+++ lib/eurorack.rb
@@ -31,17 +31,32 @@ module Eurorack
     end
   end
 
-  # # Convert a BigDecimal (or any Numeric?) to a string that shows so many decimal places (default: 4)
-  # def bd_to_s(bd, places = 4)
-  #   # Follows this structure: (bd.to_s("F") + "0000")[/.*\..{4}/]
-  #   (bd.to_s("F") + ("0" * places))[/.*\..{#{places}}/]
-  # end
-
-  def self.minimum_hp(pcb_width)
-    ((pcb_width + EURORACK_PANEL_WIDTH_REDUCTION) / EURORACK_HP).ceil.to_i
+  # Given a PCB width in mm, return the minimum HP that the PCB will fit in
+  #
+  # Input should be an Integer, Float, or BigDecimal.
+  #
+  # You can also optionally specify the amount the resulting width will be
+  # reduced by for tolerance reasons. If not specified,
+  # EURORACK_PANEL_WIDTH_REDUCTION will be used.
+  #
+  # Output is an Integer.
+  #
+  def self.minimum_hp(pcb_width_mm, reduction_mm = EURORACK_PANEL_WIDTH_REDUCTION)
+    ((pcb_width_mm.to_d + reduction_mm.to_d) / EURORACK_HP).ceil.to_i
   end
 
-  def self.panel_hp_to_mm(input)
+  # Convert a given HP value to the width of a eurack panel in mm, leaving a little room for tolerance
+  #
+  # Input can be an integer (of Integer or BigDecimal type) or a string/symbol
+  # representation see #extract_hp_from_symbol_or_string for formats).
+  #
+  # You can also optionally specify the amount the resulting width will be
+  # reduced by for tolerance reasons. If not specified,
+  # EURORACK_PANEL_WIDTH_REDUCTION will be used.
+  #
+  # Output is as a BigDecimal, to preserve floating point accuracy.
+  #
+  def self.panel_hp_to_mm(input, reduction_mm = EURORACK_PANEL_WIDTH_REDUCTION)
     case input
     when Numeric, BigDecimal
       hp = input.to_d
@@ -51,15 +66,30 @@ module Eurorack
       raise "Invalid type for hp sent to panel_hp_to_mm."
     end
     #TODO: Do the math to go from hp to mm!!!
-    (hp * EURORACK_HP) - EURORACK_PANEL_WIDTH_REDUCTION
+    (hp * EURORACK_HP) - reduction_mm
   end
 
+  # Extract the relevant HP integer from a string or symbol representation of HP.
+  #
+  # Input can be a string/symbol representation in any of the following
+  # case-insensitive configurations:
+  #   '5 hp'
+  #   '5hp'
+  #   'hp 5'
+  #   'hp5'
+  #   '5'
+  #   :hp5
+  #
+  # Output is an Integer.
+  #
   def self.extract_hp_from_symbol_or_string(eurorack_unit_string)
-    case eurorack_unit_string
+    case eurorack_unit_string.to_s.downcase
     when /\A\d+ ?hp\z/
-      hp = eurorack_unit_string[/\A(\d+) ?hp\z/, 1]
+      hp = eurorack_unit_string[/\A(\d+) ?hp\z/, 1].to_i
     when /\Ahp ?\d+\z/
-      hp = eurorack_unit_string[/\Ahp ?(\d+)\z/, 1]
+      hp = eurorack_unit_string[/\Ahp ?(\d+)\z/, 1].to_i
+    when /\A\d+\z/
+      hp = eurorack_unit_string.to_i
     else
       raise "Invalid string sent to extract_hp_from_symbol_or_string."
     end
@@ -70,6 +100,12 @@ module Eurorack
   # the_big_decimal.to_s("F")
   #NOTE: To convert from BigDecimal to a string of an accurate float with exactly 4 decimal places, use:
   # (the_big_decimal.to_s("F") + "0000")[ /.*\..{4}/ ]
+
+  # # Convert a BigDecimal (or any Numeric?) to a string that shows so many decimal places (default: 4)
+  # def bd_to_s(bd, places = 4)
+  #   # Follows this structure: (bd.to_s("F") + "0000")[/.*\..{4}/]
+  #   (bd.to_s("F") + ("0" * places))[/.*\..{#{places}}/]
+  # end
 
 end
 
