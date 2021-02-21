bc5d6e00158f3e265e70ce94e5628606f2716a45
Joe Miragliuolo <sod@llawn.com>
2021-02-21 12:46:29 -0500

    Add placeholder for Param tests

---
+++ spec/param_test.rb
@@ -0,0 +1,38 @@
+
+#TODO: Test the reversibility of Param
+
+#TODO: test with the following types of values:
+
+# 0 
+# 1 
+# 2 
+# 10
+# 11
+# 12
+# ""
+# GND
+# +5V
+# -12V
+# -5V
+# "Net-(J1-PadT)"
+# "Net-(J1-PadTN)"
+# /front_D
+# /front_C
+# 3.2
+# 0.051
+# 0.25
+# FFFFFF7F
+# 0x010f0_ffffffff
+# true
+# false
+# 15.000000
+# "gerbers/"
+# Default
+# "This is the default net class."
+# F.Paste
+# B.SilkS
+# *.Cu
+# *.Mask
+# ${KISYS3DMOD}/Button_Switch_THT.3dshapes/SW_PUSH_6mm_H9.5mm.wrl
+
+
