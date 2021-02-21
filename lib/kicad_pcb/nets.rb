90de868002fded0dba326765aae57e9ca09583b4
Joe Miragliuolo <sod@llawn.com>
2021-02-21 15:11:19 -0500

    Make Nets work with hashes

--- lib/kicad_pcb/nets.rb
+++ lib/kicad_pcb/nets.rb
@@ -3,6 +3,7 @@ require_relative 'net'
 require_relative 'render'
 
 #TODO: Don't forget about the interaction between net_classes and nets!!!!
+#       - maybe make methods in KicadPcb for #add_net and such, so it can access both classes?
 
 class KicadPcb
   class Nets
@@ -11,14 +12,47 @@ class KicadPcb
     include Render # Render contains #indent, #render_value, #render_array, and #render_hash
 
     # Forward some Hash and Enumerable methods straight to the hash
-    def_delegators :@nets, :[], :delete, :each, :include?, :key?, :length, :size
+    def_delegators :@nets, :[], :delete, :each, :include?, :key?, :keys, :length, :size
 
-    def initialize
+    def initialize(nets_hash = nil)
       @nets = {} #TODO: Or should it be an array??
+      if nets_hash
+        nets_hash.each do |_number, net_hash|
+          set_net(net_hash)
+        end
+      end
+    end
+
+    # Set a net specified by net_hash.
+    # net_hash must have the following keys: :number, :name
+    def set_net(net_hash)
+      # Index by number since name can be blank
+      @nets[net_hash[:number].to_s] = Net.new(net_hash)
+    end
+
+    def set_default_net
+      set_net({number: '0', name: ''})
     end
 
     def to_sexpr
+      output = ''
+      @nets.each do |_key, net|
+        output << net.to_sexpr
+        output << "\n"
+      end
+      return output
+    end
+
+    def to_h
+      @nets.transform_values {|value| value.to_h}
     end
 
   end
 end
+
+# Each net has a net number and a name if in the schematic the net has a label.
+#
+# (net 0 "")
+# (net 1 /SIGNAL)
+# (net 2 GND)
+
