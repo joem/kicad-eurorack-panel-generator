99888ad382b5815528706a52c4adad9fe4a7d684
Joe Miragliuolo <sod@llawn.com>
2021-02-21 19:40:22 -0500

    Minor test setup changes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,9 @@
 # at the top of each test file in the test directory with require
 # 'test_helper'.
 
+require "minitest/autorun"
+require "minitest/spec"
+# require "minitest/reporters"
+
+# Minitest::Reporters.use!
 
