ef851a5f79698bfa5be2e12648d78d2534c15359
Joe Miragliuolo <sod@llawn.com>
2021-03-05 23:32:27 -0500

    Restructure track segment tests

--- test/spec/track/segment_spec.rb
+++ test/spec/track/segment_spec.rb
@@ -12,23 +12,45 @@ describe KicadPcb::Track::Segment do
     value(Segment.new).must_be_instance_of Segment
   end
 
-  it 'generates the correct hash with #to_h' do
-    value(Segment.new.to_h).must_equal Hash[track_type: nil, start: ['', ''], end: ['', ''], layer: '', width: '', net: '', tstamp: '']
-    params1 = {start: ['35.433', '31.369'], end: ['31.623', '27.559'], width: '1', layer: 'F.Cu', net: '3'}
-    expected_hash1 = {track_type: nil, start: ['35.433', '31.369'], end: ['31.623', '27.559'], width: '1', layer: 'F.Cu', net: '3', tstamp: ''}
-    value(Segment.new(params1).to_h).must_equal expected_hash1
+  it 'has a #to_h method' do
+    value(Via.new).must_respond_to :to_h
   end
 
-  it 'generates the correct s-expression with #to_sexpr' do
-    value(Segment.new.to_sexpr).must_equal "(segment (start ) (end ) (width ) (layer ) (net ))"
+  describe '#to_h' do
 
-    params1 = {start: ['35.433', '31.369'], end: ['31.623', '27.559'], width: '1', layer: 'F.Cu', net: '3'}
-    expected_sexpr1 = '(segment (start 35.433 31.369) (end 31.623 27.559) (width 1) (layer F.Cu) (net 3))'
-    value(Segment.new(params1).to_sexpr).must_equal expected_sexpr1
+    it 'generates the correct hash' do
+      value(Segment.new.to_h).must_equal Hash[track_type: nil, start: ['', ''], end: ['', ''], layer: '', width: '', net: '', tstamp: '']
+      params1 = {start: ['35.433', '31.369'], end: ['31.623', '27.559'], width: '1', layer: 'F.Cu', net: '3'}
+      expected_hash1 = {track_type: nil, start: ['35.433', '31.369'], end: ['31.623', '27.559'], width: '1', layer: 'F.Cu', net: '3', tstamp: ''}
+      value(Segment.new(params1).to_h).must_equal expected_hash1
+    end
 
-    params2 = {start: ['35.433', '31.369'], end: ['31.623', '27.559'], width: '1', layer: 'F.Cu', net: '3'}
-    expected_sexpr2 = '(segment (start 35.433 31.369) (end 31.623 27.559) (width 1) (layer F.Cu) (net 3))'
-    value(Segment.new(params2).to_sexpr).must_equal expected_sexpr2
+    it 'passes :track_type through unchanged' do
+      value(Segment.new({track_type: nil}).to_h[:track_type]).must_be_nil
+      value(Segment.new({track_type: :segment}).to_h[:track_type]).must_equal :segment
+      value(Segment.new({track_type: 'segment'}).to_h[:track_type]).must_equal 'segment'
+    end
+
+  end
+
+  it 'has a #to_sexpr method' do
+    value(Via.new).must_respond_to :to_sexpr
+  end
+
+  describe '#to_sexpr' do
+
+    it 'generates the correct s-expression' do
+      value(Segment.new.to_sexpr).must_equal "(segment (start ) (end ) (width ) (layer ) (net ))"
+
+      params1 = {start: ['35.433', '31.369'], end: ['31.623', '27.559'], width: '1', layer: 'F.Cu', net: '3'}
+      expected_sexpr1 = '(segment (start 35.433 31.369) (end 31.623 27.559) (width 1) (layer F.Cu) (net 3))'
+      value(Segment.new(params1).to_sexpr).must_equal expected_sexpr1
+
+      params2 = {start: ['35.433', '31.369'], end: ['31.623', '27.559'], width: '1', layer: 'F.Cu', net: '3'}
+      expected_sexpr2 = '(segment (start 35.433 31.369) (end 31.623 27.559) (width 1) (layer F.Cu) (net 3))'
+      value(Segment.new(params2).to_sexpr).must_equal expected_sexpr2
+    end
+
   end
 
   it "can make an equivalent Segment with another Segment's #to_h" do
@@ -37,12 +59,6 @@ describe KicadPcb::Track::Segment do
     new = Segment.new(orig.to_h)
     value(new.to_h).must_equal orig.to_h
     value(new.to_sexpr).must_equal orig.to_sexpr
-  end
-
-  it 'passes :track_type through to #to_h unchanged' do
-    value(Segment.new({track_type: nil}).to_h[:track_type]).must_be_nil
-    value(Segment.new({track_type: :segment}).to_h[:track_type]).must_equal :segment
-    value(Segment.new({track_type: 'segment'}).to_h[:track_type]).must_equal 'segment'
   end
 
 end
