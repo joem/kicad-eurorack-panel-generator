28ab2ba9c8d91630ad2508ee3c0e7b5e68dbc4e0
Joe Miragliuolo <sod@llawn.com>
2021-03-06 19:01:30 -0500

    Fix Nets

--- lib/kicad_pcb/nets.rb
+++ lib/kicad_pcb/nets.rb
@@ -11,6 +11,8 @@ class KicadPcb
     extend Forwardable # needed for the #def_delegators forwarding
     include Render # Render contains #indent, #render_value, #render_array, and #render_hash
 
+    attr_reader :nets
+
     # Forward some Hash and Enumerable methods straight to the hash
     def_delegators :@nets, :[], :delete, :each, :include?, :key?, :keys, :length, :size
 
@@ -23,11 +25,17 @@ class KicadPcb
       end
     end
 
+    ##TODO: Implement this!
+    ##TODO: Make it auto-increment the number!
+    #def add_net(net_name)
+    #end
+
     # Set a net specified by net_hash.
     # net_hash must have the following keys: :number, :name
     def set_net(net_hash)
       # Index by number since name can be blank
       @nets[net_hash[:number].to_s] = Net.new(net_hash)
+      self # So it can be chained.
     end
 
     def set_default_net
@@ -35,12 +43,17 @@ class KicadPcb
     end
 
     def to_sexpr
-      output = ''
-      @nets.each do |_key, net|
-        output << net.to_sexpr
-        output << "\n"
+      # output = ''
+      # @nets.each do |_key, net|
+      #   output << net.to_sexpr
+      #   output << "\n"
+      # end
+      # return output
+      output_array = []
+      @nets.keys.sort.each do |key|
+        output_array << @nets[key].to_sexpr
       end
-      return output
+      return output_array.join("\n")
     end
 
     def to_h
