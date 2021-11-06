6ad8b78683e12eea82e58c562cb9e61dc90e03a0
Joe Miragliuolo <sod@llawn.com>
2021-11-06 19:18:25 -0400

    Fix indenting in writer

--- lib/kicad_pcb/writer.rb
+++ lib/kicad_pcb/writer.rb
@@ -1,8 +1,12 @@
+require_relative 'render'
+
 class KicadPcb
   class Writer
 
     # If needed, refer to section 4 in this document to see the file spec: https://kicad.org/help/legacy_file_format_documentation.pdf
     # (but most of the formatting isn't done here but in the subclasses that make up the parts of the KicadPcb object)
+
+    include Render # Render contains #indent, #render_value, #render_array, and #render_hash
 
     def initialize(kicad_pcb_object)
       @kicad_pcb_object = kicad_pcb_object
@@ -13,25 +17,25 @@ class KicadPcb
       # header contains the opening parentheses (but should it???) #TODO: maybe change this?
       the_output << @kicad_pcb_object.header.to_sexpr
       the_output << conditional_newline(str: the_output.last, newlines: 2)
-      # the_output << @kicad_pcb_object.general.to_sexpr
+      # the_output << indent(@kicad_pcb_object.general.to_sexpr, 2)
       # the_output << conditional_newline(str: the_output.last)
-      the_output << @kicad_pcb_object.page.to_sexpr
+      the_output << indent(@kicad_pcb_object.page.to_sexpr, 2)
       the_output << conditional_newline(str: the_output.last)
-      the_output << @kicad_pcb_object.layers.to_sexpr
+      the_output << indent(@kicad_pcb_object.layers.to_sexpr, 2)
       the_output << conditional_newline(str: the_output.last, newlines: 2)
-      the_output << @kicad_pcb_object.setup.to_sexpr
+      the_output << indent(@kicad_pcb_object.setup.to_sexpr, 2)
       the_output << conditional_newline(str: the_output.last, newlines: 2)
-      the_output << @kicad_pcb_object.nets.to_sexpr
+      the_output << indent(@kicad_pcb_object.nets.to_sexpr, 2)
       the_output << conditional_newline(str: the_output.last, newlines: 2)
-      the_output << @kicad_pcb_object.net_classes.to_sexpr
+      the_output << indent(@kicad_pcb_object.net_classes.to_sexpr, 2)
       the_output << conditional_newline(str: the_output.last, newlines: 2)
-      # the_output << @kicad_pcb_object.parts.to_sexpr
+      # the_output << indent(@kicad_pcb_object.parts.to_sexpr, 2)
       # the_output << conditional_newline(str: the_output.last)
-      the_output << @kicad_pcb_object.graphic_items.to_sexpr
+      the_output << indent(@kicad_pcb_object.graphic_items.to_sexpr, 2)
       the_output << conditional_newline(str: the_output.last, newlines: 2)
-      the_output << @kicad_pcb_object.tracks.to_sexpr
+      the_output << indent(@kicad_pcb_object.tracks.to_sexpr, 2)
       the_output << conditional_newline(str: the_output.last)
-      # the_output << @kicad_pcb_object.zones.to_sexpr
+      # the_output << indent(@kicad_pcb_object.zones.to_sexpr, 2)
       # the_output << conditional_newline(str: the_output.last)
       the_output << ')' # final closed parentheses on last line
       return the_output.join('')
