d720daa2ddfcf8d7beb6108ea721d862ba6a7725
Joe Miragliuolo <sod@llawn.com>
2021-11-08 02:08:05 -0500

    Remove duplicate methods from Page

--- lib/kicad_pcb/page.rb
+++ lib/kicad_pcb/page.rb
@@ -6,11 +6,6 @@ class KicadPcb
 
     # include Render # Render contains #indent, #render_value, #render_array, and #render_hash
 
-    def initialize
-    end
-
-    def to_sexpr
-    end
     def initialize(page = 'A4')
       @page = Param[page]
     end
